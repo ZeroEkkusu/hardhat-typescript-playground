31be2dbd6b83ee85bf2e53b10beb31cfe291c99b
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-06-21 20:26:49 +0200

    ✨ Add Fund button

--- html-fund-me/index.js
+++ html-fund-me/index.js
@@ -1,14 +1,40 @@
+// cannot use `require`
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     try {
       await window.ethereum.request({ method: "eth_requestAccounts" })
-      document.getElementById("connectButton").innerHTML = "Connected"
-      const accounts = await ethereum.request({ method: "eth_accounts" })
+      connectButton.innerHTML = "Connected"
+      const accounts = await window.ethereum.request({ method: "eth_accounts" })
       console.log(accounts)
     } catch (error) {
       console.log(error)
     }
   } else {
-    document.getElementById("connectButton").innerHTML = "No wallet"
+    connectButton.innerHTML = "No wallet"
+  }
+}
+
+async function fund() {
+  const ethAmount = "1"
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      })
+      console.log(`Funding with ${ethAmount} ETH...`)
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
